c1536a3e25d04599e7b37fc2bb84d39431fbaf54
jmgallego <jmgallego@106-09.info2>
2022-09-21 14:41:57 +0200

    Primer commit

---
+++ Exercise2.3/src/exercise23/Exercise23.java
@@ -0,0 +1,22 @@
+package exercise23;
+
+import java.util.Scanner;
+
+public class Exercise23 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Introduzca su año de nacimiento: ");
+		Scanner age = new Scanner(System.in);
+		int ageint = age.nextInt();
+		System.out.println();
+		System.out.print("Introduzca el año actual: ");
+		Scanner year = new Scanner(System.in);
+		int yearint = year.nextInt();
+		/*int age2 = ageint + 1;
+		System.out.println();
+		System.out.println("El año que viene el mismo día su edad será: " + age2 + " años");
+		age.close();*/
+	}
+
+}
