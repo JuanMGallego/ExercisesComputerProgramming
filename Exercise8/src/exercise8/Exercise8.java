a081beb8607fd13a34e88e15d332096ecc6f57e5
jmgallego <jmgallego@106-09.info2>
2022-09-21 13:42:22 +0200

    Primer commit

---
+++ Exercise8/src/exercise8/Exercise8.java
@@ -0,0 +1,16 @@
+package exercise8;
+
+import java.util.Scanner;
+
+public class Exercise8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.print("Introduzca un número: ");
+			Scanner n1 = new Scanner(System.in);
+			int n2 = n1.nextInt();
+			System.out.println();
+			System.out.println("Su número es: " + n2);
+	}
+
+}
