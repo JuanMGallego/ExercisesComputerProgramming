0f8e2a1962ec9d2414fa40a9d97bb22379d03b6d
jmgallego <jmgallego@106-09.info2>
2022-09-21 13:40:23 +0200

    Primer commit

---
+++ Exercise6/src/exercise6/Exercise6.java
@@ -0,0 +1,10 @@
+package exercise6;
+
+public class Exercise6 {
+	public static void main(String[] args) {
+		System.out.println("Name: Juan Manuel");
+		System.out.println("Adress: Verde Que Te Quiero Verde, 22");
+		System.out.println();
+		System.out.println("Telephone Number: 601409351");
+	}
+}
