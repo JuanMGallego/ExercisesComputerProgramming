93c3542e494f4472d8c7f5ab1dea2ea8b99709c3
jmgallego <jmgallego@106-09.info2>
2022-09-22 13:59:15 +0200

    First Version Finished

--- Exercise2.3/src/exercise23/Exercise23.java
+++ Exercise2.3/src/exercise23/Exercise23.java
@@ -6,17 +6,34 @@ public class Exercise23 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		//Scanner is started to introduce data into variables. Then variables are declared.
+		Scanner sc = new Scanner(System.in);
+		int byear = sc.nextInt();
+		int year = sc.nextInt();
+		
+		//Born year is asked
 		System.out.print("Introduzca su año de nacimiento: ");
-		Scanner age = new Scanner(System.in);
-		int ageint = age.nextInt();
 		System.out.println();
+		
+		//Actual year is asked
 		System.out.print("Introduzca el año actual: ");
-		Scanner year = new Scanner(System.in);
-		int yearint = year.nextInt();
-		/*int age2 = ageint + 1;
 		System.out.println();
-		System.out.println("El año que viene el mismo día su edad será: " + age2 + " años");
-		age.close();*/
+		
+		//age of user is calculated in two ways	
+		int age = year - byear;
+		int age2 = year - byear - 1;
+
+		if (age2 <= 0) { //If the result turns illogical, it gives an error
+			System.out.println("Ha introducido mal los datos, vuelva a introducirlos");
+
+		} else { //But if everything goes fine, it prints the two results
+			System.out.println("Si ya fue su cumpleaños, su edad es de " + age + " años");
+			System.out.println("Si todavía no fue su cumpleaños, su edad es de " + age2 + " años");
+			
+			sc.close(); //scanner closes
+		}
+	
 	}
 
 }
