13fe07d9ff554075cdc7bcd031b321aa8fb8697b
jmgallego <jmgallego@106-09.info2>
2022-09-22 14:36:52 +0200

    Renamed

---
+++ Exercise1.1/src/exercise11/Exercise.java
@@ -0,0 +1,11 @@
+package exercise11;
+
+public class Exercise {
+	
+	public static void main(String[] args) {
+		
+		//Message is printed
+		System.out.print("Hello, how are you? ");
+		System.out.println("Fine thanks.");
+	}
+}
