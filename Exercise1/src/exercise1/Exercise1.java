2af8b2af4e3d07a7712693f4e775ad5d4b1bc7fb
jmgallego <jmgallego@106-09.info2>
2022-09-22 14:23:37 +0200

    Renamed

--- Exercise1/src/exercise1/Exercise1.java
+++ Exercise1/src/exercise1/Exercise1.java
@@ -1,7 +1,10 @@
 package exercise1;
 
 public class Exercise1 {
+	
 	public static void main(String[] args) {
+		
+		//Message is printed
 		System.out.print("Hello, how are you? ");
 		System.out.println("Fine thanks.");
 	}
