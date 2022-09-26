81374614af11b34cbb31dce15c6961d38064d472
jmgallego <jmgallego@106-09.info2>
2022-09-26 10:10:23 +0200

    First commit

--- Exercises2/src/exercises2/Exercise1.java
+++ Exercises2/src/exercises2/Exercise1.java
@@ -1,5 +1,20 @@
 package exercises2;
 
-public class Exercise1 {
+import java.util.Scanner;
 
+public class Exercise1 {
+	
+	public static void main (String[] args) {
+		
+		Scanner keyboard = new Scanner(System.in);
+		final int YEAR = 2022;
+		int age, bornIn;
+		System.out.print("How old are you this year?");
+		age = keyboard.nextInt();
+		bornIn = YEAR - age;
+		System.out.println("I think you were born in " + bornIn);
+		keyboard.close();
+		}
+
+	
 }
