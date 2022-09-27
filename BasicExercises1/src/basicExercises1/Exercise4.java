be9c8e9d56e584fd1fe74b8e6e621f25a5926d43
jmgallego <jmgallego@106-09.info2>
2022-09-27 11:13:06 +0200

    First commit

--- BasicExercises1/src/basicExercises1/Exercise4.java
+++ BasicExercises1/src/basicExercises1/Exercise4.java
@@ -1,10 +1,32 @@
 package basicExercises1;
 
+import java.util.Scanner;
+
 public class Exercise4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		float m1;
+		float m2;
+		float mm;
+		
+		System.out.println("Programa para calcular la media aritmética de dos notas.");
+		System.out.println();
+		
+		System.out.print("Introduce la nota nº1: ");
+		m1 = sc.nextFloat();
+		
+		System.out.print("Introduce la nota nº2: ");
+		m2 = sc.nextFloat();
+		System.out.println();
+		
+		mm = (m1 + m2) / 2;
+		System.out.println("Su nota media es: " + mm);
+		
+		sc.close();
+		
 	}
 
 }
--- BasicExercises1/src/basicExercises1/Exercise5.java
+++ BasicExercises1/src/basicExercises1/Exercise5.java
@@ -1,10 +1,30 @@
 package basicExercises1;
 
+import java.util.Scanner;
+
 public class Exercise5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		double r;
+		double l;
+		double a;
+		
+		System.out.println("Aplicación para calcular longitud y área de una circunferencia.");
+		System.out.println();
+		
+		System.out.print("Introduzca el radio de la circunferencia: ");
+		r = sc.nextDouble();
+		System.out.println();
+		
+		l = 2 * Math.PI * r;
+		
+		System.out.println("La longitud de su circunferencia es de " + l " u");
+		
+		
+		
 	}
 
 }
