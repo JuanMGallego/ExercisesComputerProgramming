0f8901ae30bdd3b3af82c38b75169b0c6315330c
jmgallego <jmgallego@106-09.info2>
2022-09-28 14:00:31 +0200

    updated

--- BasicExercises1/src/basicExercises1/Exercise10.java
+++ BasicExercises1/src/basicExercises1/Exercise10.java
@@ -2,30 +2,30 @@ package basicExercises1;
 
 import java.util.Scanner;
 
-public class Exercise9 {
+public class Exercise10 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		//Scanner is started to introduce data into variables, then variables are declared
 		Scanner sc = new Scanner(System.in);
-		float pts, eur;
-		final double PTA = 0.006;
+		float gross, net;
+		final double IVA = 0.79;
 		
 		//title
-		System.out.println("CONVERSOR DE PESETAS A EUROS");
+		System.out.println("CONVERSOR DE PRECIO BRUTO A NETO");
 		System.out.println();
 		
-		//pesetas amount is asked
-		System.out.print("Introduzca una cantidad en pesetas: ");
-		pts = sc.nextFloat();
+		//gross price
+		System.out.print("Introduzca su precio bruto: ");
+		gross = sc.nextFloat();
 		System.out.println();
 		
-		//pesetas are converted to euros using the costant
-		eur = (float)(pts*PTA);
+		//net price is calculated using the constant IVA
+		net = (float)(gross*IVA);
 		
 		//final result is printed
-		System.out.println(pts + " pesetas = " + eur + " euros.");
+		System.out.println("El precio neto es de " + net + " euros");
 		
 		sc.close(); //scanner closes
 		
